8ad96993d734b8c2572965a5d253015aa20fdbd1
Gupta_A <83379728+neur0n-7@users.noreply.github.com>
2025-04-01 15:30:10 -0700

    Update main.py

--- main.py
+++ main.py
@@ -68,4 +68,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=os.environ.get('PORT', 8080), debug=True)
